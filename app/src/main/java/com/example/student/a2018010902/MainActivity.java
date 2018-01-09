fd814c5ada043dab9fbd005708032f24b9d6fffc
miowoo <miowoo821@gmail.com>
2018-01-09 16:37:17 +0800

    版本相容作法

--- app/src/main/java/com/example/student/a2018010902/MainActivity.java
+++ app/src/main/java/com/example/student/a2018010902/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.student.a2018010902;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,21 +14,42 @@ public class MainActivity extends AppCompatActivity {
     NotificationChannel channellove;
     NotificationManager nm;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        channellove=new NotificationChannel(idlover,"CLOVE",NotificationManager.IMPORTANCE_HIGH);
+        nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+       if(Build.VERSION.SDK_INT>=26){
+           regchannel();
+       }
+    }
+
+   @TargetApi(Build.VERSION_CODES.O)
+    public void regchannel() {
+        channellove = new NotificationChannel(idlover, "CLOVE", NotificationManager.IMPORTANCE_HIGH);
         channellove.setDescription("敘述1");
         channellove.enableLights(true);
         channellove.enableVibration(true);
 
-        nm=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
         nm.createNotificationChannel(channellove);
-    }
+        }
+
+        @TargetApi(26)
+        @SuppressWarnings("deprecation")
+
+
         public void click1(View v){
-            Notification.Builder builder=new Notification.Builder(MainActivity.this,idlover);
+        Notification.Builder builder;
+        if(Build.VERSION.SDK_INT>=26) {
+            builder=new Notification.Builder(MainActivity.this,idlover);
+        }
+        else{
+            builder = new Notification.Builder(MainActivity.this);
+        }
 
             builder.setContentTitle("標題"); //這三行必填，是通知的內容
             builder.setContentText("內容"); //這三行必填，是通知的內容
