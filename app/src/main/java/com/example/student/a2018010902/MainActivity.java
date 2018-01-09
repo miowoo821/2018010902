4c9601ef98ee6c41460a56baaa9d197bcd02f6b6
miowoo <miowoo821@gmail.com>
2018-01-09 16:49:54 +0800

    可點擊通知前往該頁面

--- app/src/main/java/com/example/student/a2018010902/MainActivity.java
+++ app/src/main/java/com/example/student/a2018010902/MainActivity.java
@@ -4,6 +4,8 @@ import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -51,9 +53,15 @@ public class MainActivity extends AppCompatActivity {
             builder = new Notification.Builder(MainActivity.this);
         }
 
+            Intent it=new Intent(MainActivity.this,Main2Activity.class);
+             it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            PendingIntent pi=PendingIntent.getActivity(MainActivity.this,123, it,PendingIntent.FLAG_UPDATE_CURRENT);
             builder.setContentTitle("標題"); //這三行必填，是通知的內容
             builder.setContentText("內容"); //這三行必填，是通知的內容
             builder.setSmallIcon(R.drawable.ic_launcher_background); //這三行必填，是通知的內容
+
+            builder.setAutoCancel(true);
+            builder.setContentIntent(pi);
 
             Notification notify=builder.build();
             nm.notify(1,notify);
